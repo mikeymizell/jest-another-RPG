ee73c8fba8bc5826530aec68783003ae4579af6a
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-12-13 15:13:28 -0800

    player object and player object test (which pass)

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,16 @@
-const { TestWatcher } = require('jest');
 const Player = require('../lib/Player');
 
-TestWatcher('creates a player object', () => {
+jest.mock('../lib/Potion');
+
+test('creates a player object', () => {
     const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqua(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number))
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 })
